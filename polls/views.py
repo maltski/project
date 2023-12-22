3d1bc43d26b12a7922b38862b8ab18ff3b1a4732
maltski <malte.enlund@abo.fi>
2023-12-22 19:58:55 +0200

    .

--- polls/views.py
+++ polls/views.py
@@ -32,7 +32,7 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
-def mystery():
+def mystery(request):
     raise Exception("This is a deliberate error to test DEBUG setting")
 
 
